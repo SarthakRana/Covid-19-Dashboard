10956874ce389366978fbe7df7a9f53ecd0ffdf4
Sarthak Rana <sarthak6246@gmail.com>
2020-10-02 12:12:32 +0530

    modified credential entry

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-mkdir -p ~/.streamlit
-
-echo "[server]
-headless = true
-enableCORS = false
-port = $PORT
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
